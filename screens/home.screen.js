15e2df08e3be9ad650c5d06434e36f98b4f5dc3c
Naieem Mahmud Supto <naieemsupto@gmail.com>
2018-12-28 02:52:29 +0600

    chat added to home screen

--- screens/home.screen.js
+++ screens/home.screen.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import {
     View,
     StyleSheet,
@@ -14,10 +14,11 @@ import {
     ImageBackground
 } from 'react-native';
 import GridList from 'react-native-grid-list';
-import {Icon, Button} from 'react-native-elements'
+import { Icon, Button } from 'react-native-elements'
 import BasicImageSlider from '../ReusableComponents/ImageSlider';
-import {RouteController} from '../ReusableComponents/RouteControll/route.controller';
-import {db} from '../DB/config';
+import { RouteController } from '../ReusableComponents/RouteControll/route.controller';
+import { db } from '../DB/config';
+import { Header } from 'react-navigation';
 
 const items = [
     {
@@ -47,7 +48,8 @@ export class HomeScreen extends Component {
             categoryInfo: [],
             backupCatInfo: [],
             modalVisible: false,
-            contacts: []
+            currentText: '',
+            messages: []
         };
     }
     componentDidMount() {
@@ -60,7 +62,7 @@ export class HomeScreen extends Component {
                 .state
                 .backupCatInfo
                 .slice(0, 11);
-            this.setState({categoryInfo: sliceCat});
+            this.setState({ categoryInfo: sliceCat });
             console.log(this.state.categoryInfo.length);
         });
     }
@@ -71,12 +73,12 @@ export class HomeScreen extends Component {
             categoryName: catName
         });
     }
-    renderItem = ({item, index}) => {
+    renderItem = ({ item, index }) => {
         return (
             <TouchableHighlight
                 style={{
-                padding: 5
-            }}
+                    padding: 5
+                }}
                 onPress={() => this.gotoListPage(item.title)}>
                 {/* <Image
                     style={{
@@ -87,27 +89,38 @@ export class HomeScreen extends Component {
                 }}/> */}
                 <ImageBackground
                     source={{
-                    uri: item.picture
-                }}
+                        uri: item.picture
+                    }}
                     style={{
-                    width: '100%',
-                    height: 70,
-                    justifyContent: 'center',
-                    alignItems: 'center'
-                }}>
+                        width: '100%',
+                        height: 70,
+                        justifyContent: 'center',
+                        alignItems: 'center'
+                    }}>
                     <Text
                         style={{
-                        color: '#fff',
-                        fontWeight: 'bold',
-                        fontSize: 25,
-                        textTransform: 'uppercase'
-                    }}>{item.title}</Text>
+                            color: '#fff',
+                            fontWeight: 'bold',
+                            fontSize: 25,
+                            textTransform: 'uppercase'
+                        }}>{item.title}</Text>
                 </ImageBackground>
             </TouchableHighlight>
         );
     };
     setModalVisible(visible) {
-        this.setState({modalVisible: visible});
+        this.setState({ modalVisible: visible });
+    }
+    saveMessage = (message) => {
+        console.log('before add length is ' + this.state.messages.length);
+        this.state.messages.push({ message: message });
+        this.state.messages.reverse();
+        this.setState({
+            messages: this.state.messages,
+            currentText: ''
+        });
+        console.log(this.state.messages);
+        console.log('after add length is ' + this.state.messages.length);
     }
     render() {
         const images = ['https://placeimg.com/640/640/nature', 'https://placeimg.com/640/640/cats', 'https://placeimg.com/640/640/cats', 'https://placeimg.com/640/640/cats'];
@@ -121,57 +134,84 @@ export class HomeScreen extends Component {
                         numColumns={2}
                         renderItem={this.renderItem}
                         style={{
-                        paddingTop: 5
-                    }}/>
+                            paddingTop: 5
+                        }} />
                 </ScrollView>
-                {/* bottom fav button starts */}
+                {/* chat modal */}
+                <Modal
+                    animationType="slide"
+                    transparent={false}
+                    visible={this.state.modalVisible}
+                    onRequestClose={() => {
+                        Alert.alert('Modal has been closed.');
+                    }}>
+                    <KeyboardAvoidingView enabled behavior='padding' style={{ height: '100%' }}>
+                        <ScrollView>
+                            <View style={{ flex: 1, padding: 20, flexDirection: 'column', justifyContent: 'center', alignContent: 'center', alignItems: 'center', paddingTop: 50 }}>
+                                {/* close button container starts */}
+                                <View style={{ position: 'absolute', top: 25, right: 5 }}>
+                                    <Icon
+                                        reverse
+                                        name='ios-close'
+                                        type='ionicon'
+                                        color='#517fa4'
+                                        onPress={() => {
+                                            this.setModalVisible(!this.state.modalVisible);
+                                        }}
+                                    />
+                                </View>
+                                {/* close button container ends */}
+                                {/* message container starts */}
+                                <View style={{ marginTop: 50, minHeight: 200, borderWidth: 1, borderColor: '#000', padding: 10, width: '100%' }}>
+                                    <Text style={{ fontWeight: 'bold', paddingBottom: 10 }}>Messages</Text>
+                                    {this.state.messages.length > 0 && this.state.messages.map((data, index) => {
+                                        return (<Text key={index} style={{ paddingBottom: 10 }}>{data.message}</Text>);
+                                    })}
+                                </View>
+                                {/* message container ends */}
+                            </View>
+
+                        </ScrollView>
+                        {/* message sending input part */}
+                        <View style={{ justifyContent: 'center', alignItems: 'center', alignContent: 'center' }}>
+                            <TextInput
+                                style={{
+                                    height: 40,
+                                    borderColor: '#000',
+                                    borderWidth: 1,
+                                    width: 300,
+                                    paddingLeft: 20,
+                                    borderRadius: 10
+                                }}
+                                placeholder="Your Message"
+                                placeholderTextColor="#000"
+                                selectionColor="#000"
+                                onChangeText={(text) => {
+                                    console.log(text);
+                                    this.setState({
+                                        currentText: text
+                                    });
+                                }} value={this.state.currentText} />
+                            <TouchableHighlight style={{ paddingTop: 10, paddingBottom: 20 }}>
+                                <Button raised title='BUTTON' onPress={() => this.saveMessage(this.state.currentText)} />
+                            </TouchableHighlight>
+                        </View>
+
+                    </KeyboardAvoidingView>
+                </Modal>
+                {/* Fav Icon */}
                 <TouchableHighlight
                     style={{
-                    position: 'absolute',
-                    bottom: 10,
-                    right: 20
-                }}
+                        position: 'absolute',
+                        bottom: 10,
+                        right: 20
+                    }}
                     onPress={() => {
-                    this.setModalVisible(true);
-                }}>
-                    <Icon reverse name='ios-american-football' type='ionicon' color='#517fa4'/>
+                        this.setModalVisible(true);
+                    }}>
+                    <Icon reverse name='ios-american-football' type='ionicon' color='#517fa4' />
                 </TouchableHighlight>
-                {/* bottom fav button ends */}
-                {/* modal body design starts */}
-                <ScrollView>
-                    <Modal
-                        animationType="slide"
-                        transparent={false}
-                        visible={this.state.modalVisible}>
-                        <KeyboardAvoidingView
-                            style={{
-                            flex: 1
-                        }}
-                            behavior="padding"
-                            enabled>
-                            <TouchableHighlight
-                                style={{
-                                justifyContent: 'center',
-                                alignContent: 'center',
-                                alignItems: 'center'
-                            }}>
-                                <TextInput
-                                    style={{
-                                    height: 40,
-                                    borderColor: 'red',
-                                    borderWidth: 1,
-                                    width: '100%'
-                                }}
-                                    placeholder="Type here to translate!"
-                                    onChangeText={() => {
-                                    console.log('new personsssssss')
-                                }}/>
-                                <Button title='BUTTON'/>
-                            </TouchableHighlight>
-                        </KeyboardAvoidingView>
-                    </Modal>
-                </ScrollView>
-                {/* modal body design ends */}
+
             </SafeAreaView>
         )
     }
@@ -184,5 +224,4 @@ const styles = StyleSheet.create({
         padding: 10
     }
 });
-
 export default HomeScreen;
